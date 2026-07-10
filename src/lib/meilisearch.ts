0c97a431f841fb11e78735030589bb783aa4b0eb
Kaiser9888 <seu.email@example.com>
2026-07-10 09:13:39 -0300

    fix: adiciona lib meilisearch

---
+++ src/lib/meilisearch.ts
@@ -0,0 +1,9 @@
+export async function searchProducts(query: string) {
+    const res = await fetch(`https://imperium-bikes.onrender.com/api/search/products?q=${query}`);
+    return res.json();
+}
+
+export async function searchUsers(query: string) {
+    const res = await fetch(`https://imperium-bikes.onrender.com/api/search/users?q=${query}`);
+    return res.json();
+}
